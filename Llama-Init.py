189da6e3bc98122fc76c704f20de20df9f4747d3
Hirithik R <74068473+Zaphkiel-Hi@users.noreply.github.com>
2024-01-31 13:48:39 +0800

    Update Llama-Init.py

--- Llama-Init.py
+++ Llama-Init.py
@@ -1,1 +1,78 @@
 
+
+model_name_or_path = "TheBloke/WizardLM-1.0-Uncensored-Llama2-13B-GPTQ"
+model_basename = "model"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+
+model = AutoGPTQForCausalLM.from_quantized(
+    model_name_or_path,
+    revision="gptq-4bit-128g-actorder_True",
+    model_basename=model_basename,
+    use_safetensors=True,
+    trust_remote_code=True,
+    inject_fused_attention=False,
+    device=DEVICE,
+    quantize_config=None,
+)
+     
+
+DEFAULT_SYSTEM_PROMPT = """
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible. Please ensure that your responses are as accurate as given in the PDF and contain Vehicle Registration.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+""".strip()
+
+
+def generate_prompt(prompt: str, system_prompt: str = DEFAULT_SYSTEM_PROMPT) -> str:
+    return f"""
+[INST] <>
+{system_prompt}
+<>
+
+{prompt} [/INST]
+""".strip()
+     
+
+streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+     
+
+text_pipeline = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=1024,
+    temperature=0,
+    top_p=0.95,
+    repetition_penalty=1.15,
+    streamer=streamer,
+)
+     
+
+llm = HuggingFacePipeline(pipeline=text_pipeline, model_kwargs={"temperature": 0})
+     
+
+SYSTEM_PROMPT = "Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer."
+
+template = generate_prompt(
+    """
+{context}
+
+Question: {question}
+""",
+    system_prompt=SYSTEM_PROMPT,
+)
+     
+
+prompt = PromptTemplate(template=template, input_variables=["context", "question"])
+     
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=db.as_retriever(search_kwargs={"k": 2}),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": prompt},
+)
+     
+
