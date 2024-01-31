163abc6a471f2277e85dd6c0bfd5db2dbeb455b7
Hirithik R <74068473+Zaphkiel-Hi@users.noreply.github.com>
2024-01-31 13:49:29 +0800

    Update pdfques.py

--- pdfques.py
+++ pdfques.py
@@ -1,1 +1,29 @@
 
+
+result = qa_chain("Please retrieve key value such as policy holder , insured company etc in json format")
+     
+
+
+result = qa_chain("Please provide the agent code")
+     
+
+
+result = qa_chain("I just wanna say that you have been very helpful and I wish the same in future")
+     
+
+
+result = qa_chain("Whats the name of the company in the pdf")
+     
+
+
+result = qa_chain("Check on the bottom left corner of the page and list the page numerical value")
+     
+
+
+result = qa_chain("In the Vehicle Details section, where it appears as the Vehicle Registration Number, find its complete value with last 4 digits too")
+     
+
+
+result = qa_chain("What is the address of UNITED INDIA INSURANCE COMPANY LIMITED as clearly stated inside the pdf")
+     
+
